fe40c19dee8ada2eb8cb05bcf5ed0ae4134158be
Guillaume Alpe <guillaume.alpe@ansys.com>
2022-03-22 11:55:32 +0100

    Add scraper for Immo Du Particulier

--- App/App.js
+++ App/App.js
@@ -2,48 +2,26 @@ import * as propertyComparer from "./PropertyComparer.js"
 import * as notification from "./Notification.js"
 import * as laForet from "./Scrapers/LaForet.js"
 import * as orpi from "./Scrapers/Orpi.js"
+import * as immoDuParticulier from "./Scrapers/ImmoDuParticulier.js"
 
 console.log(`Let the scraping commence...`);
 
-const laForetProperties = laForet.scrapeData()
-    .then((liveProperties) => {
-        const savedProperties = laForet.readProperties();
-        console.log(`Scraped La Foret a bunch... fround ${liveProperties.length} properties. Had ${savedProperties.length} saved.`);
+const laForetProperties = 
+    laForet.scrapeData()
+        .then(handleScrapedData(laForet, "La Foret"))
+        .catch(err => console.error(err));
 
-        const diff = propertyComparer.symmetricDifference(savedProperties, liveProperties);
+const orpiProperties = 
+    orpi.scrapeData()
+        .then(handleScrapedData(orpi, "Orpi"))
+        .catch(err => console.error(err));
 
-        if(diff.propertiesAdded.length > 0 || diff.propertiesRemoved.length > 0) {
-            laForet.saveProperties(liveProperties, diff);
-        } else {
-            console.log("Nothing new from La Foret.")
-        }
+const immoDuParticulierProperties = 
+    immoDuParticulier.scrapeData()
+        .then(handleScrapedData(immoDuParticulier, "Immo Du Particulier"))
+        .catch(err => console.error(err));
 
-        return { 
-            agency: "La Foret", 
-            newProperties: diff.propertiesAdded
-        };
-    }).catch(err => console.error(err));
-
-const orpiProperties = orpi.scrapeData()
-    .then((liveProperties) => {
-        const savedProperties = orpi.readProperties();
-        console.log(`Scraped Orpi a bunch... fround ${liveProperties.length} properties. Had ${savedProperties.length} saved.`);
-
-        const diff = propertyComparer.symmetricDifference(savedProperties, liveProperties);
-
-        if(diff.propertiesAdded.length > 0 || diff.propertiesRemoved.length > 0) {
-            orpi.saveProperties(liveProperties, diff);
-        } else {
-            console.log("Nothing new from Orpi.")
-        }
-
-        return { 
-            agency: "Orpi", 
-            newProperties: diff.propertiesAdded
-        };
-    }).catch(err => console.error(err));
-
-const scrapers = [laForetProperties, orpiProperties];
+const scrapers = [laForetProperties, orpiProperties, immoDuParticulierProperties];
 const potentialHouses = await Promise.allSettled(scrapers)
     .then((results) => {
         let allNewProperties = [];
@@ -64,3 +42,23 @@ if(potentialHouses.length > 0) {
 } 
 
 console.log(`Scraping done!`);
+
+function handleScrapedData(scraper, agencyName) {
+    return (liveProperties) => {
+        const savedProperties = scraper.readProperties()
+        console.log(`Scraped ${agencyName} a bunch... fround ${liveProperties.length} properties. Had ${savedProperties.length} saved.`)
+
+        const diff = propertyComparer.symmetricDifference(savedProperties, liveProperties)
+
+        if (diff.propertiesAdded.length > 0 || diff.propertiesRemoved.length > 0) {
+            scraper.saveProperties(liveProperties, diff);
+        } else {
+            console.log(`Nothing new from ${agencyName}.`)
+        }
+
+        return {
+            agency: agencyName,
+            newProperties: diff.propertiesAdded
+        }
+    }
+}
---
+++ App/Scrapers/ImmoDuParticulier.js
@@ -0,0 +1,37 @@
+import * as webScraper from "../WebScraper.js"
+import * as propertiesRepo from "../PropertiesRepo.js"
+import * as cheerio from 'cheerio';
+
+const baseUrl = "https://www.immoduparticulier.com";
+const url = baseUrl+"/catalog/advanced_search_result.php?action=update_search&search_id=1727994553829177&map_polygone=&C_28_search=EGAL&C_28_type=UNIQUE&C_28=Vente&C_28_tmp=Vente&C_27_search=EGAL&C_27_type=TEXT&C_27=2&C_27_tmp=2&C_34_MIN=80&C_34_search=COMPRIS&C_34_type=NUMBER&C_30_search=COMPRIS&C_30_type=NUMBER&C_30_MAX=500000&C_65_search=CONTIENT&C_65_type=TEXT&C_65=13170+LES-PENNES-MIRABEAU%2C13700+MARIGNANE%2C13880+VELAUX%2C13127+VITROLLES&C_65_tmp=13170+LES-PENNES-MIRABEAU&C_65_tmp=13700+MARIGNANE&C_65_tmp=13880+VELAUX&C_65_tmp=13127+VITROLLES&keywords=&C_30_MIN=&C_33_search=COMPRIS&C_33_type=NUMBER&C_33_MIN=&C_33_MAX=&C_34_MAX=&C_36_MIN=&C_36_search=COMPRIS&C_36_type=NUMBER&C_36_MAX=&C_38_MAX=&C_38_MIN=&C_38_search=COMPRIS&C_38_type=NUMBER&C_47_type=NUMBER&C_47_search=COMPRIS&C_47_MIN=&C_94_type=FLAG&C_94_search=EGAL&C_94=";
+const dataFileName = "ImmoDuParticulier";
+
+async function scrapeData() {
+    const rawData = await webScraper.getRawData(url);
+    // parsing the data
+    const $ = cheerio.load(rawData);
+    let propertiesHtml = $(".item-product");
+    
+    let properties = [];
+    propertiesHtml.each((i, el) => {
+        let property = { title:"", price:""};
+        property.link = baseUrl + $(el).find(".products-link").children("a").attr("href").replace("..", "");
+        property.title = $(el).find(".products-name").text().trim();
+        property.price = $(el).find(".products-price").text().trim();
+        properties.push(property);
+    });
+
+    return properties;
+}
+
+function readProperties() {
+    return propertiesRepo.readProperties(dataFileName);
+}
+
+function saveProperties(liveProperties, diff) {
+    propertiesRepo.createChangeLog(dataFileName, diff);
+    propertiesRepo.updateProperties(dataFileName, liveProperties);
+}
+
+
+export { scrapeData, readProperties, saveProperties };
