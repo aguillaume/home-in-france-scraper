7ade8f311de679c4d6266291a45b4af4d66a8171
Guillaume Alpe <guillaumealpe@hotmail.com>
2022-03-25 15:10:50 +0100

    add ejs and ui for current properties

--- Server/app.js
+++ Server/app.js
@@ -4,23 +4,45 @@ import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { Worker } from 'worker_threads';
+import createError from 'http-errors';
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 
 
 const fileName = fileURLToPath(import.meta.url);
+const dirName = path.dirname(fileName);
 
 let app = express();
+
+// view engine setup
+app.set('views', path.join(dirName, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(path.dirname(fileName), '../public')));
+app.use(express.static(path.join(dirName, '../public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 /**
  * Get port from environment and store in Express.
@@ -51,15 +73,15 @@ function normalizePort(val) {
 }
 
 function startScraper() {
-    try {
-        var myWorker = new Worker('./MainLoop.js');
-        myWorker.onmessage = function(e) {
-            console.log(`Message received from worker: ${e.data}`);
-        }
-        
-    } catch (error) {
-        console.log(error);
-    }
+    // try {
+    //     var myWorker = new Worker('./MainLoop.js');
+    //     myWorker.onmessage = function (e) {
+    //         console.log(`Message received from worker: ${e.data}`);
+    //     }
+
+    // } catch (error) {
+    //     console.log(error);
+    // }
 }
 
 export default app;
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,9 +1,26 @@
 import { Router } from 'express';
+import * as propertiesRepo from "../../App/PropertiesRepo.js"
+
 var router = Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const laForetData = propertiesRepo.readProperties("laForetProperties");
+  const ImmoDuParticulierData = propertiesRepo.readProperties("ImmoDuParticulier");
+  const orpiPropertiesData = propertiesRepo.readProperties("orpiProperties");
+
+  const agencies = [{
+    name: "La Foret",
+    properties: laForetData
+  },{
+    name: "Immo Du Particulier",
+    properties: ImmoDuParticulierData
+  },{
+    name: "Orpi",
+    properties: orpiPropertiesData
+  }]
+
+  res.render('index', { title: 'Home in France Scraper' , agenciesData: agencies});
 });
 
 export default router;
