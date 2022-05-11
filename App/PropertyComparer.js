fb5bef42e66ff2613db746c8cc0108864b3404cc
Guillaume <guillaumealpe@hotmail.com>
2022-05-11 21:19:16 +0200

    remove readded properties

--- App/PropertyComparer.js
+++ App/PropertyComparer.js
@@ -4,12 +4,9 @@ function symmetricDifference(context, oldProperties, newProperties, propertyHist
     ctx = context ?? console;
     let removed = [];
     let added = [];
-    let reAdded = [];
 
     oldProperties.forEach(oldProp => {
         if(!newProperties.find(np => np.link == oldProp.link)) {
-            // ctx.log(`Property ${oldProp.title} has been removed.`)
-            // ctx.log(JSON.stringify(oldProp, null, 2));
             removed.push(oldProp);
         }
     })
@@ -18,15 +15,15 @@ function symmetricDifference(context, oldProperties, newProperties, propertyHist
         const key = `${newProp.link}||${newProp.price}`;
 
         if(!oldProperties.find(np => np.link == newProp.link)) {
-            if(propertyHistory.has(key)) {
-                reAdded.push({
-                    notes: `This property has been added before, see comparison between 'now' and 'before' `,
-                    now: newProp,
-                    before: propertyHistory.get(key)
-                })
+            if(propertyHistory.has(key) ) {
+                let savedProp = propertyHistory.get(key);
+                if(savedProp.price != newProp.price) {
+                    savedProp.oldPrice =savedProp.price
+                    savedProp.price = newProp.price
+                    savedProp.notes = `This property price has been updated`,
+                    added.push(savedProp);
+                }
             } else {
-                // ctx.log(`Property ${newProp.title} is new!!!.`)
-                // ctx.log(JSON.stringify(newProp, null, 2));
                 added.push(newProp);
             }
         }
@@ -34,8 +31,7 @@ function symmetricDifference(context, oldProperties, newProperties, propertyHist
 
     return {
         propertiesAdded: {
-            new: added,
-            reAdded: reAdded
+            new: added
         },
         propertiesRemoved: removed
     }
